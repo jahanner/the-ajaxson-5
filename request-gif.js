81af9ad8e435e405ef3e0b70743c03ac55ad3d82
Jesse Hanner <jahanner7@gmail.com>
2017-03-30 18:39:56 -0500

    Almost done

--- request-gif.js
+++ request-gif.js
@@ -1,5 +1,4 @@
-
-
+var answer = '5';
 $(document).ready(function() {
     // register our function as the "callback" to be triggered by the form's submission event
     $("#form-gif-request").submit(fetchAndDisplayGif); // in other words, when the form is submitted, fetchAndDisplayGif() will be executed
@@ -13,7 +12,6 @@ $(document).ready(function() {
  * upon receiving a response from Giphy, updates the DOM to display the new GIF
  */
 function fetchAndDisplayGif(event) {
-
     // This prevents the form submission from doing what it normally does: send a request (which would cause our page to refresh).
     // Because we will be making our own AJAX request, we dont need to send a normal request and we definitely don't want the page to refresh.
     event.preventDefault();
@@ -28,30 +26,32 @@ function fetchAndDisplayGif(event) {
     };
 
     // make an ajax request for a random GIF
+    var url = "https://api.giphy.com/v1/gifs/random";
     $.ajax({
-        url: "https://api.giphy.com/v1/gifs/random" // TODO where should this request be sent?
-        data: {
-            api_key,
-            tag,
-        },
+        url: url, // TODO where should this request be sent?
+        data: params,
              // attach those extra parameters onto the request
         success: function(response) {
             // if the response comes back successfully, the code in here will execute.
-
             // jQuery passes us the `response` variable, a regular javascript object created from the JSON the server gave us
             console.log("we received a response!");
             console.log(response);
-
             // TODO
             // 1. set the source attribute of our image to the image_url of the GIF
-            $("#gif").src = url + params.api_key + params.tag;
+            $("#gif").attr('src', response.data.image_url);
             // 2. hide the feedback message and display the image
-            $("#gif").hidden("false");
-            $("feedback").hidden("true");
+            if ($(".validate").val() != answer){
+                $("#feedback").text("No GIFs for you.");
+                $("#feedback").css("color", "red");
+                setGifLoadedStatus(false);
+            }
+            else {
+            setGifLoadedStatus(true);
+            $(".span-validate").hide();
+            }
         },
         error: function() {
             // if something went wrong, the code in here will execute instead of the success function
-
             // give the user an error message
             $("#feedback").text("Sorry, could not load GIF. Try again!");
             setGifLoadedStatus(false);
@@ -60,7 +60,7 @@ function fetchAndDisplayGif(event) {
 
     // TODO
     // give the user a "Loading..." message while they wait
-    $("#feedback").hidden("false");
+    setGifLoadedStatus(false);
     $("#feedback").text("Loading...");
 
 }
